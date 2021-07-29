6e6ba4b442b3c6078184c5b3dc3b908e31cde280
Rutvik2207 <78476201+Rutvik2207@users.noreply.github.com>
2021-07-29 22:26:35 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,10 @@
+class Launcher{
+   constructor(bodyA, pointB){
+         var options = {
+             bodyA: bodyA,
+             pointB: pointB,
+             stiffness: 0.5           
+         } 
+   
+    }
+   }
